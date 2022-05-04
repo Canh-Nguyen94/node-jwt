7cf769370f2b857f856ccc54495067d451ce4bb4
Canh Nguyen <canh.nguyen@madison-technology.com>
2022-05-04 17:26:50 +0700

    handle sign up and login with jwt

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,7 +1,9 @@
 const User = require("../models/User")
+const jwt = require("jsonwebtoken")
 
 //handle errors
 const handleErrors = (err)=>{
+    console.log(err.message, err.code)
     let errors = {email: "", password: "",}
     //validate duplicate email addresses
     if(err.code === 11000){
@@ -9,12 +11,20 @@ const handleErrors = (err)=>{
         return errors;
     }
     //validate errors
-    if(err.message.includes("user validation failed")){
-        Object.values(err.errors).forEach(({properties})=>{
-            errors[properties.path] = properties.message
-        })
+    if(err.message === "incorrect email"){
+        errors.email = "that email is not registered"
+    }
+    if(err.message === "password is incorrect"){
+        errors.password = "that password is incorrect"
     }
     return errors
+}
+
+const maxAge = 3*24*60*60
+const createToken = (id)=>{
+    return jwt.sign({id},"kevin secret",{
+        expiresIn: maxAge
+    } )
 }
 const signup_get = (req,res)=>{
     res.render('signup')
@@ -23,7 +33,9 @@ const signup_post = async (req,res)=>{
     const {email, password} = req.body;
    try {
     const user =  await User.create({email, password});
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, {httpOnly: true, maxAge: maxAge*1000});
+    res.status(201).json({user:user._id});
    } catch (err) {
        const errors = handleErrors(err)
        
@@ -34,10 +46,20 @@ const signup_post = async (req,res)=>{
 const login_get = (req,res)=>{
     res.render("login")
 }
-const login_post = (req,res)=>{
+const login_post = async (req,res)=>{
     const {email, password} = req.body;
-    console.log(email, password)
-    res.send("user login")
+    
+    try {
+        const user = await User.login(email, password)
+        const token = createToken(user._id);
+    res.cookie("jwt", token, {httpOnly: true, maxAge: maxAge*1000});
+        res.status(200).json({user: user._id})
+    } catch (error) {
+        const errors = handleErrors(error)
+        
+        res.status(400).json({ errors})
+        
+    }
 }
 
 module.exports ={signup_get, signup_post, login_get, login_post}
--- models/User.js
+++ models/User.js
@@ -1,22 +1,42 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const {isEmail} = require("validator")
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
-    email:{
-        type:String,
-        require: [true, "please enter an email"],
-        unique: true,
-        lowercase:true,
-        validate: [isEmail, "email is not valid"]
-    },
-    password:{
-        type:String,
-        require: [true, "please enter a password"],
-        minlength:[6, "minimum password is at least 6 characters"]
-    },
-})
+  email: {
+    type: String,
+    require: [true, "please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "email is not valid"],
+  },
+  password: {
+    type: String,
+    require: [true, "please enter a password"],
+    minlength: [6, "minimum password is at least 6 characters"],
+  },
+});
+//fire a function after a doc saved to db
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-const User = mongoose.model("user", userSchema)
+//static method to login user
+userSchema.statics.login = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("password is incorrect");
+  }
+  throw Error("incorrect email");
+};
 
-module.exports = User
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
